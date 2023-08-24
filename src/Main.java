2bf990c1e1ddf0832f1c0aff29a0e7e3aa34f7f7
mjosepc <mjose.powell@gmail.com>
2023-08-24 10:33:36 -0400

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-            System.out.printf("");
+public class Main{
+    public static void main(String[] args){
+        int [][] matriz;
+    }
+    public static void ValidacionMatriz(String[] args){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese una matriz MxN");
+        System.out.println("Introduzca un número para m:");
+        int m = leer.nextInt();
+        System.out.println("Introduzca un número para n:");
+        int n = leer.nextInt();
+        if (m > 0 && n > 0){
+            System.out.println("True");
+        } else {
+            System.out.println("False");
         }
     }
 }
