32fb10a3b1777a145953f389cf95865f7b720ed0
mjosepc <mjose.powell@gmail.com>
2023-08-24 11:14:49 -0400

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        menu();
+    }
+    public static void Menu() {
+        Scanner leer = new Scanner(System.in);
+        int M , N;
         int [][] matriz;
+
+        while (true) {
+            System.out.println("Menú:");
+            System.out.println("1. Ingresar dimensiones de la matriz");
+            System.out.println("2. Crear matriz");
+            System.out.println("3. Llenar matriz");
+            System.out.println("4. Mostrar fila");
+            System.out.println("5. Verificar si la matriz es de TIPO CERO");
+            System.out.println("6. Salir");
+            System.out.print("Elija una opción: ");
+            int opcion = leer.nextInt();
+
     }
     public static void ValidacionMatriz(String[] args){
         Scanner leer = new Scanner(System.in);
@@ -16,6 +33,12 @@ public class Main{
         } else {
             System.out.println("False");
 
+
         }
     }
+    public static void crearmatriz(){
+        int[][] matriz = new int [m][n];
+
+    }
 }
+
